9a2c76ff64112308d8bd5c84cf4baa5bd59244a4
Lynn Mumia <lynn.mumia@ashesi.edu.gh>
2017-01-20 17:47:05 +0000

    Multiply

--- Calc.java
+++ Calc.java
@@ -1,10 +1,21 @@
-public class CommandLineCalculator{
-  public static void main(String[]args){
-    if(args[0].equals("add")){
-      double one=Double.parseDouble(args[1]);
-      double two=Double.parseDouble(args[2]);
-      System.out.print(one+two);
-    }
-    
-  }
-}
+	public class CommandLineCalculator{
+	  public static void main(String[]args){
+		if(args[0].equals("add")){
+		  double one=Double.parseDouble(args[1]);
+		  double two=Double.parseDouble(args[2]);
+		  System.out.print(one+two);
+		  System.out.print("");
+		}    
+		else if(args[1].equals("subtract")){
+		  double one=Double.parseDouble(args[1]);
+		  double two=Double.parseDouble(args[2]);
+		  System.out.print(one-two);
+		}
+		else if(args[1].equals("multiply")){
+		  double one=Double.parseDouble(args[1]);
+		  double two=Double.parseDouble(args[2]);
+		  System.out.print(one*two);
+		}
+		}
+	  }
+	}
