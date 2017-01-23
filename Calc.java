14d692148fc637076f0f4e5168aa90d18d0976db
Lynn Mumia <lynn.mumia@ashesi.edu.gh>
2017-01-23 10:06:51 +0000

    Addition

--- Calc.java
+++ Calc.java
@@ -1,21 +1,14 @@
-	public class CommandLineCalculator{
-	  public static void main(String[]args){
-		if(args[0].equals("add")){
-		  double one=Double.parseDouble(args[1]);
-		  double two=Double.parseDouble(args[2]);
-		  System.out.print(one+two);
-		  System.out.print("");
-		}    
-		else if(args[0].equals("subtract")){
-		  double one=Double.parseDouble(args[1]);
-		  double two=Double.parseDouble(args[2]);
-		  System.out.print(one-two);
-		}
-		else if(args[0].equals("multiply")){
-		  double one=Double.parseDouble(args[1]);
-		  double two=Double.parseDouble(args[2]);
-		  System.out.print(one*two);
-		}
-		}
-	  }
-	}
+public class Calc{
+ 
+  public static void main(String[] args){
+  
+  String symbol = args[0];
+  double num1 = Integer.parseInt(args[1]);
+  double num2 = Integer.parseInt(args[2]);
+
+if(symbol.equals("add")){
+  System.out.println();
+  System.out.println(" ***********Simple calculator********");
+System.out.println("The sum is " + (num1 + num2));
+   }
+   }
